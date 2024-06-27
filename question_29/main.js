5214393cbf3d5dcc3e18ff906c92e092ce0ee838
abdullahahtesham <146721363+abdullahahtesham@users.noreply.github.com>
2024-06-27 20:21:50 +0500

    add this

---
+++ question_29/main.js
@@ -0,0 +1,13 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+//Favorite Fruit:
+let favorite_fruits = ["Apple", "Banana", "Pinapple"];
+if (favorite_fruits.includes("Apple")) {
+    console.log(`You really like Apple!`);
+}
+if (favorite_fruits.includes("Banana")) {
+    console.log(`You really like Banana!`);
+}
+if (favorite_fruits.includes("Pinapple")) {
+    console.log(`You really like Pinapple!`);
+}
